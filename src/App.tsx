6507690c1dfde2c42612798ee355a572cf7d2927
Yurii Stenin <yuriy.stenin@appnovation.com>
2022-04-15 01:02:12 +0300

    Added material ui styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
-import React, { useState, useEffect } from 'react'
-import './App.scss';
+import React, {useState, useEffect} from 'react'
 import Result from './components/Result';
-import { adjectives } from './words/adjectives'
-import { nouns } from './words/nouns'
+import {adjectives} from './words/adjectives'
+import {nouns} from './words/nouns'
+import {Grid, Button, Typography} from "@mui/material";
 
 export default function App() {
 
-  const [adjectivesList, setAdjectivesList] = useState<string[]>([]);
-  const [nounsList, setNounsList] = useState<string[]>([]);
-  const [sprintName, setSprintName] = useState<string>("");
+    const [adjectivesList, setAdjectivesList] = useState<string[]>([]);
+    const [nounsList, setNounsList] = useState<string[]>([]);
+    const [sprintName, setSprintName] = useState<string>("");
 
-  useEffect(() => {
-    setAdjectivesList(adjectives);
-    setNounsList(nouns);
-  }, [])
+    useEffect(() => {
+        setAdjectivesList(adjectives);
+        setNounsList(nouns);
+    }, [])
 
-  const handleGenerate = () => {
-    const selectedAdjIndex = Math.floor(Math.random() * adjectivesList.length)
-    const selectedNounIndex = Math.floor(Math.random() * nounsList.length)
+    const handleGenerate = () => {
+        const selectedAdjIndex = Math.floor(Math.random() * adjectivesList.length)
+        const selectedNounIndex = Math.floor(Math.random() * nounsList.length)
 
-    setSprintName(`${adjectivesList[selectedAdjIndex]} ${nounsList[selectedNounIndex]}`)
-}
+        setSprintName(`${adjectivesList[selectedAdjIndex]} ${nounsList[selectedNounIndex]}`)
+    }
 
 
-  return (
-    <div className="App">
-      <div className="content">
-         <h3>Sprint Name Generator</h3>
-        <button className='btn-prime' onClick={handleGenerate}>Generate</button>
-        <Result text={sprintName}/>
-      </div>
-    </div>
-  );
+    return (
+        <Grid direction="column" justifyContent="center" alignItems="center" container spacing={0} style={{minHeight: '100vh'}}>
+            <Grid item p={2}>
+                <Typography variant="h4" align="center">
+                    Sprint Name Generator
+                </Typography>
+            </Grid>
+            <Grid item p={2}>
+                <Button variant='contained' onClick={handleGenerate}>Generate</Button>
+            </Grid>
+            <Grid item>
+                <Typography variant="h3" align="center">
+                    <Result text={sprintName}/>
+                </Typography>
+            </Grid>
+        </Grid>
+    );
 }
--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import ReactTooltip from 'react-tooltip'
+import {Typography} from "@mui/material";
 
 interface IProps {
-  text: string;
+    text: string;
 }
 
-export default function Result({ text }:IProps) {
-  return (
-    <div className="result">{text}</div>
-  )
+export default function Result({text}: IProps) {
+    function copyToClipboard(text: string) {
+        navigator.clipboard.writeText(text).then(() => {
+            ReactTooltip.rebuild();
+        });
+    }
+    return (
+        <div>
+            <Typography variant="h2" onClick={() =>{copyToClipboard(text);}} data-tip="Copied!" data-event='click focus'>{text}</Typography>
+            <ReactTooltip globalEventOff='click' />
+        </div>
+
+)
 }
