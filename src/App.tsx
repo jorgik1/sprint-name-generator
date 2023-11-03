1b740c007d64d4988aebe1f40bdd4179a304b40e
Yuriy Stenin <yuriy.ipcom@gmail.com>
2023-11-03 21:35:35 +0000

    Open ai lib

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Result from './components/Result';
 import {adjectives} from './words/adjectives'
 import {nouns} from './words/nouns'
 import {Grid, Button, Typography, TextField} from "@mui/material";
+import { OpenAI } from "openai";
 
 export default function App() {
 
@@ -36,42 +37,49 @@ export default function App() {
     }
 
     const handleGenerateAI = async () => {
-        const response = await fetch('https://api.openai.com/v1/completions',
-            {
-                headers: {
-                    'Authorization': `Bearer ${openAIKey}`,
-                    'Content-Type': 'application/json'
-                },
-                method: 'POST',
-                body: JSON.stringify({ prompt: 'Generate a sprint name using an adjective and a noun.' })
-            }
-        );
+        const openai = new OpenAI({
+            'apiKey': openAIKey,
+            dangerouslyAllowBrowser: true
+        })
+        const response = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [{"role": "system", "content": "You are a helpful assistant to generate a sprint names"},
+                {"role": "user", "content": "Generate just one sprint name with nouns and ajective"}],
+          });
+          if (response && response.choices && response.choices.length > 0) {
+            const choises = response.choices
+            if (choises) {
+                const sprintName = choises[0].message.content?.trim() || ''
+                setSprintName(sprintName);
 
-        const data = await response.json();
-        if(data.choices && data.choices.length > 0) {
-            setSprintName(data.choices[0].text.trim());
-        }
-    }
+            }
+         }   
+    
+    }    
 
     return (
         <Grid direction="column" justifyContent="center" alignItems="center" container spacing={0} style={{minHeight: '100vh'}}>
-            <Grid item p={4}>
-                <Typography variant="h4" align="center">
-                    Sprint Name Generator
-                </Typography>
-            </Grid>
-            <Grid item p={4}>
-                <TextField label="OpenAI Key" value={openAIKey} onChange={updateOpenAIKey} />
-            </Grid>
-            <Grid item p={4}>
-                <Button variant='contained' onClick={handleGenerate}>Generate</Button>
-                <Button variant='contained' onClick={handleGenerateAI}>Generate with AI</Button>
-            </Grid>
-            <Grid item>
-                <Typography variant="h4" align="center">
-                    <Result text={sprintName}/>
-                </Typography>
-            </Grid>
+        <Grid item p={4}>
+            <Typography variant="h4" align="center">
+                Sprint Name Generator
+            </Typography>
         </Grid>
+        <Grid item p={4}>
+            <TextField label="OpenAI Key" value={openAIKey} onChange={updateOpenAIKey} fullWidth />
+        </Grid>
+        <Grid container item p={2} justifyContent="space-between">
+        <Grid item xs={12} sm={5}>
+            <Button variant="contained" onClick={handleGenerate} fullWidth style={{margin: '10px'}}>Generate</Button>
+        </Grid>
+        <Grid item xs={12} sm={5}>
+            <Button variant="contained" onClick={handleGenerateAI} fullWidth style={{margin: '10px'}}>Generate with AI</Button>
+        </Grid>
+        </Grid>
+        <Grid item>
+            <Typography variant="h4" align="center">
+                <Result text={sprintName}/>
+            </Typography>
+        </Grid>
+    </Grid>
     );
 }
